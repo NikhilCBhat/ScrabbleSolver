b0d5eac7cb5b0b01fa7144af3b13b27da0459b01
nikhilcbhat <bhatnikhil314@gmail.com>
2019-06-02 20:26:06 -0400

    Added a blankboard function, and other convienence changes

--- ocr.py
+++ ocr.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from collections import Counter
 import threading
+from wordFunctions import makeBlankBoard
 
 ## Image --> Image
 ## Resizes an image
@@ -15,19 +16,18 @@ def resize(img):
 ## Gets the letters the player has
 def getPlayerLetters(screenshot, display=False):
     height, width, _ = screenshot.shape
-    # print(height, width)
     screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
     screenshot = screenshot[int(3*height/4):height, 0:width]
-
     screenshot = cv2.threshold(screenshot, 50, 255, cv2.THRESH_BINARY_INV)[1]
     screenshot = cv2.bitwise_not(screenshot)
     screenshot = cv2.GaussianBlur(screenshot,(3,3),0)
     screenshot = cv2.resize(screenshot, (0,0), None, 2.5, 2.5)
-    # print(list(pytesseract.image_to_string(screenshot)))
+
     while display:
         cv2.imshow('t', resize(screenshot))
         if(cv2.waitKey(1) & 0xFF == ord('q')):
             break
+
     return list(pytesseract.image_to_string(screenshot))
 
 squareSize = 75
@@ -37,14 +37,20 @@ def getSquare(img, x, y):
     square = cv2.cvtColor(oGsquare, cv2.COLOR_BGR2GRAY)
     square = cv2.threshold(square, 210, 255, cv2.THRESH_BINARY_INV)[1]
     square = cv2.GaussianBlur(square,(1,1),0)
-    square = np.tile(square, 20)
+    square = np.tile(square, 25)
     letters = pytesseract.image_to_string(square)
     data = Counter(letters)
+
+    while True:
+        cv2.imshow('hi', square)
+        cv2.waitKey(1)
+
     return str(data.most_common(1)[0][0]) if len(letters) else "-"
 
 def returnLetters(filePath):
     start = time.time()
     img = cv2.imread(filePath)
+    # print(img)
     opposite = cv2.bitwise_not(img)
     print("~~~Gameboard~~~ \n")
     for i in range(15):
@@ -57,4 +63,20 @@ def returnLetters(filePath):
     print("\nYour letters are %s."%''.join(yourletters))
     print(time.time() - start)
 
-returnLetters('lateGame.PNG')
+def getBoardLetters(filePath):
+    img = cv2.imread(filePath)
+    board = makeBlankBoard(15)
+    opposite = cv2.bitwise_not(img)
+    for i in range(15):
+        for j in range(15):
+            board[i][j] = getSquare(opposite, i, j)
+    return board
+
+if __name__ == '__main__':
+    # img = cv2.imread('lateGame.PNG')
+    # print(getPlayerLetters(img))
+    img = cv2.imread('scrabbleGame.jpg')
+
+    getSquare(img, 0,9)
+
+    # returnLetters('lateGame.PNG')
