7f38e3628ef71e132a604be5194876eca5982eec
nikhilcbhat <bhatnikhil314@gmail.com>
2019-07-29 21:25:21 -0400

    Added transpose function, added fileLocation at the top

--- wordUtils.py
+++ wordUtils.py
@@ -1,3 +1,6 @@
+import os
+parent = os.path.dirname(os.getcwd())
+
 pointsList = {'-': -1, 'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
  'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
 
@@ -17,10 +20,10 @@ MODIFIERS =    [['~', '~', '~', 'TW', '~', '~', 'TL', '~', 'TL', '~', '~', 'TW',
                 ['~', '~', 'DL', '~', '~', 'DW', '~', '~', '~', 'DW', '~', '~', 'DL', '~', '~'],
                 ['~', '~', '~', 'TW', '~', '~', 'TL', '~', 'TL', '~', '~', 'TW', '~', '~', '~']]
 
-letterMultiplier = {"xDL": 2, "xTL":3}
-wordMultiplier = {"xTW":3, "xDW":2}
+letterMultiplier = {"DL": 2, "TL":3}
+wordMultiplier = {"TW":3, "DW":2}
 
-with open("scrabble.txt") as file: 
+with open(parent + "/scrabble.txt") as file: 
    fileWords = file.readlines() 
    fileWords = set([x[:-1] for x in fileWords])
 
@@ -46,3 +49,8 @@ def getLetters(tiles):
     for tile in tiles:
         letters += tile.letter
     return letters
+
+## Transposes a board
+## [List-of Lists] --> [List-of Lists]
+def transpose(board):
+    return list(map(list, zip(*board)))
